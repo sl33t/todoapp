3daf2c860613aeebfd8ebc9673a9a2603d422130
Ricky Catron <catron.ricky@gmail.com>
2016-09-20 14:21:09 -0400

    Added user model

---
+++ web/models/user.ex
@@ -0,0 +1,19 @@
+defmodule Todoapp.User do
+  use Todoapp.Web, :model
+
+  schema "users" do
+    field :name, :string
+    field :password, :string
+
+    timestamps()
+  end
+
+  @doc """
+  Builds a changeset based on the `struct` and `params`.
+  """
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, [:name, :password])
+    |> validate_required([:name, :password])
+  end
+end
