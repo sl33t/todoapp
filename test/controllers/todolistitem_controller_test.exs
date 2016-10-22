ba01826da71c443d5d4f5de563a0d059b117fb7a
Ricky Catron <catron.ricky@gmail.com>
2016-10-21 23:53:05 -0400

    Changed name of index test

--- test/controllers/todolistitem_controller_test.exs
+++ test/controllers/todolistitem_controller_test.exs
@@ -9,7 +9,7 @@ defmodule Todoapp.TodolistitemControllerTest do
     assert String.contains?(search_text, target), ~s(Expected #{inspect search_text} to contain target)
   end
 
-  test "lists all entries on index", %{conn: conn} do
+  test "ensure homepage loads and title is correct", %{conn: conn} do
     conn = get conn, "/"
     assert_contains html_response(conn, 200), "Homepage"
   end
