fe6997c8a63f7f8fb0732abd7f3e4cd3c8293311
Ricky Catron <catron.ricky@gmail.com>
2016-10-22 19:43:39 -0400

    User logged in for testing and creation with valid data passes

--- test/controllers/todolistitem_controller_test.exs
+++ test/controllers/todolistitem_controller_test.exs
@@ -2,22 +2,42 @@ defmodule Todoapp.TodolistitemControllerTest do
   use Todoapp.ConnCase
 
   alias Todoapp.Todolistitem
-  @valid_attrs %{text: "some content", order_by: 0}
+  alias Todoapp.User
+  alias Todoapp.Repo
+  @valid_attrs %{text: "some content"}
   @invalid_attrs %{}
 
   def assert_contains(search_text, target) do
     assert String.contains?(search_text, target), ~s(Expected #{inspect search_text} to contain target)
   end
 
+  def guardian_login(user, token \\ :token, opts \\ []) do
+    build_conn()
+      |> bypass_through(Todoapp.Router, [:browser])
+      |> get("/")
+      |> Guardian.Plug.sign_in(user, token, opts)
+      |> send_resp(200, "Flush the session yo")
+      |> recycle()
+  end
+
+  setup do
+    user = Repo.insert!(%User{name: "name", oauth_id: "id", avatar: "avatar", email: "email@email.com"})
+    :ok
+  end
+
   test "creates resource and redirects when data is valid", %{conn: conn} do
-    conn = post conn, todolistitem_path(conn, :create), todolistitem: @valid_attrs
-    assert redirected_to(conn) == todolistitem_path(conn, :index)
+    user = Repo.get_by(User, name: "name")
+    conn = guardian_login(user)
+    |> post(todolistitem_path(conn, :create), todolistitem: @valid_attrs)
+    assert_contains json_response(conn, 200)["flash_message"], "Item created successfully."
     assert Repo.get_by(Todolistitem, @valid_attrs)
   end
 
   test "does not create resource and renders errors when data is invalid", %{conn: conn} do
-    conn = post conn, todolistitem_path(conn, :create), todolistitem: @invalid_attrs
-    assert_contains html_response(conn, 200), "Item failed to create."
+    user = Repo.get_by(User, name: "name")
+    conn = guardian_login(user)
+    |> post(todolistitem_path(conn, :create), todolistitem: @invalid_attrs)
+    assert_contains json_response(conn, 200)["flash_message"], "Item created successfully."
   end
 
   test "updates chosen resource and redirects when data is valid", %{conn: conn} do
