d4bdfe20a00116bcf8c2d43f008fc68b614d5bb3
Ricky Catron <catron.ricky@gmail.com>
2016-10-22 21:28:13 -0400

    Delete test passes

--- test/controllers/todolistitem_controller_test.exs
+++ test/controllers/todolistitem_controller_test.exs
@@ -9,7 +9,7 @@ defmodule Todoapp.TodolistitemControllerTest do
   @invalid_attrs %{}
 
   def assert_contains(search_text, target) do
-    assert String.contains?(search_text, target), ~s(Expected #{inspect search_text} to contain target)
+    assert String.contains?(search_text, target), ~s(Expected #{inspect search_text} to contain #{inspect target})
   end
 
   def guardian_login(user, token \\ :token, opts \\ []) do
@@ -51,18 +51,30 @@ defmodule Todoapp.TodolistitemControllerTest do
     conn = guardian_login(user)
     |> put(todolistitem_path(conn, :update, todolistitem), todolistitem: @valid_attrs2)
     assert Repo.get_by(Todolistitem, @valid_attrs2)
+    assert_contains json_response(conn, 200)["flash_message"], "Item updated successfully."
   end
 
   test "does not update chosen resource and renders errors when data is invalid", %{conn: conn} do
-    todolistitem = Repo.insert! %Todolistitem{}
-    conn = put conn, todolistitem_path(conn, :update, todolistitem), todolistitem: @invalid_attrs
-    assert_contains html_response(conn, 200), "Item failed to update."
+    user = Repo.get_by(User, name: "name")
+    conn = guardian_login(user)
+    |> post(todolistitem_path(conn, :create), todolistitem: @valid_attrs)
+
+    user = Repo.preload(user, todolistitems: from(todolistitem in Todolistitem))
+    todolistitem = List.first(user.todolistitems)
+    conn = guardian_login(user)
+    |> put(todolistitem_path(conn, :update, todolistitem), todolistitem: @invalid_attrs)
+    assert_contains json_response(conn, 200)["flash_message"], "Item failed to update."
   end
 
   test "deletes chosen resource", %{conn: conn} do
-    todolistitem = Repo.insert! %Todolistitem{}
-    conn = delete conn, todolistitem_path(conn, :delete, todolistitem)
-    assert redirected_to(conn) == todolistitem_path(conn, :index)
+    user = Repo.get_by(User, name: "name")
+    conn = guardian_login(user)
+    |> post(todolistitem_path(conn, :create), todolistitem: @valid_attrs)
+
+    user = Repo.preload(user, todolistitems: from(todolistitem in Todolistitem))
+    todolistitem = List.first(user.todolistitems)
+    conn = guardian_login(user)
+    |> delete(todolistitem_path(conn, :delete, todolistitem))
     refute Repo.get(Todolistitem, todolistitem.id)
   end
 end
