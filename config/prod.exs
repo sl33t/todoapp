8322f9d7ded9a59b160b3676aa05668447475f29
Ricky Catron <catron.ricky@gmail.com>
2017-05-29 14:40:31 -0400

    Changed .Repo to .Web.Repo

--- config/prod.exs
+++ config/prod.exs
@@ -21,7 +21,7 @@ config :todoapp, Todoapp.Web.Endpoint,
 # Do not print debug messages in production
 config :logger, level: :info
 
-config :todoapp, Todoapp.Repo,
+config :todoapp, Todoapp.Web.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
